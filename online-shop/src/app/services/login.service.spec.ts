f0c0a5b2fc6c868bdd3f7c7ac49f6f299a864e8e
juliaiakab <julia_julcsi@yahoo.com>
2021-04-27 12:44:47 +0300

    fixed the shopping cart view

--- online-shop/src/app/services/login.service.spec.ts
+++ online-shop/src/app/services/login.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { User } from '../interfaces/userInterface';
 
 import { LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let user = {} as User;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -12,5 +14,12 @@ describe('LoginService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#login should return value from observable', (done: DoneFn) => {
+    service.login((user = {} as User)).subscribe((value) => {
+      expect(value).toBe('observable value');
+      done();
+    });
   });
 });
